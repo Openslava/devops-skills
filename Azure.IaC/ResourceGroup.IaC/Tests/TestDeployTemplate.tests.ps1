2a099cba41657b540b7a0a672c4f1dd75babdde3
Viliam Batka Name <viliam@batka.name>
2017-10-03 12:36:22 -0700

    test that template is valid

--- Azure.IaC/ResourceGroup.IaC/Tests/TestDeployTemplate.tests.ps1
+++ Azure.IaC/ResourceGroup.IaC/Tests/TestDeployTemplate.tests.ps1
@@ -1,11 +1,12 @@
 ﻿# Author: Viliam Batka
 
-Describe "TestDeployTemplate" {
-    Context "TestRun" {
-        It "DefaultValues Validate Only" {
-            {
-                & "$PSScriptRoot\..\Deploy-AzureResourceGroup.ps1" -ValidateOnly
-            }  | Should Not Throw
+Describe "Test ARM Template" {
+    Context "Test Run" {
+        It "Deploy with Default Values" {
+            $Error.Clear()
+            $result = . "$PSScriptRoot\..\Deploy-AzureResourceGroup.ps1" -ValidateOnly
+            $Error.Count | Should Be 0
+            ($result  -match 'Template is valid') | Should be $true
         }
     }
 }
